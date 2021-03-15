c402243eb02fe4398079ff1cd635ae07c1a9b635
cuiyp <cuiyaping58@sina.com>
2021-03-15 16:16:25 +0800

    test: webpack base test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -9,7 +9,7 @@ describe('webpack.base.js test case', () => {
 
     // eslint-disable-next-line no-undef
     it('entry', () => {
-      assert.equal(baseConfig.entry.index, '/Users/apple/Documents/Study/Demo/Webpack/builder-webpack/test/smoke/template/src/index/index.js')
-      assert.equal(baseConfig.entry.search, '/Users/apple/Documents/Study/Demo/Webpack/builder-webpack/test/smoke/template/src/search/index.js')
+      assert.equal(baseConfig.entry.index.indexOf('/smoke/template/src/index/index.js') > -1, true)
+      assert.equal(baseConfig.entry.search.indexOf('smoke/template/src/search/index.js') > -1, true)
     });
 });
